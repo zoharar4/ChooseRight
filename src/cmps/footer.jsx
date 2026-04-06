b2b071ab09066610ce2c2445063f66575062b024
Zohar <zoharar4@gmail.com>
2026-04-06 13:02:58 +0300

    footer

--- src/cmps/footer.jsx
+++ src/cmps/footer.jsx
@@ -1,9 +1,10 @@
-import logo from "../../public/logo.svg"
+import logo from "../../public/logo.png"
 export function Footer() {
 
     return (
         <footer>
             <div className="container">
+
                 <div className="section1">
                     <div className="logo-name-container">
                         <img src={logo} alt="logo" />
@@ -12,24 +13,63 @@ export function Footer() {
                             <h3>שמרית בן עמי</h3>
                         </div>
                     </div>
-
                     <div className="up-btn">
-                        <button>חזרה למעלה</button>
+                        <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+                            חזרה למעלה
+                        </button>
                     </div>
                 </div>
-        <hr />
                 <div className="section2">
-                    <p>רשתות חברתיות</p>
-                    <div>
-                        <i className="fa-brands fa-square-instagram"></i>
-                        <i className="fa-brands fa-square-facebook"></i>
+
+                    <div className="social-container">
+                        <p>רשתות חברתיות</p>
+                        <div className="social-icons">
+                            <a
+                                href="https://www.instagram.com/lbhwr.nkwn.smryt.bn.my"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="social-icon instagram"
+                                aria-label="Instagram">
+                                <i className="fa-brands fa-square-instagram"></i>
+                            </a>
+                            <a
+                                href="https://www.facebook.com/lbhwr.nkwn.smryt.bn.my"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="social-icon facebook"
+                                aria-label="Facebook"
+                            >
+                                <i className="fa-brands fa-square-facebook"></i>
+                            </a>
+                        </div>
                     </div>
-                    <p>מייל - shimritben1@gmail.com</p>
+
+                    <div className="contact-container">
+
+                        <div className="mail-container">
+                            <i className="fa-solid fa-envelope"></i>
+                            <a href="" className="contact-link">
+                                shimritben1@gmail.com
+                            </a>
+                        </div>
+                        <div className="phone-container">
+                            <i className="fa-solid fa-phone"></i>
+                            <a className="contact-link">
+                                +972-54-588-1616
+                            </a>
+                            {/* <a href="tel:+972545881616" className="contact-link">
+                                +972-54-588-1616
+                            </a> */}
+                        </div>
+
+                    </div>
+
                 </div>
-            </div>
+            </div >
+
             <div className="section3">
                 <p>כל הזכויות שמורות</p>
             </div>
-        </footer>
+        </footer >
     )
 }
